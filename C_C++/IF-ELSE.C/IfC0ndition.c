194e01da366b8cf6981c0014cd30710bcb45b52d
WasifBanday <wasifahmad679@gmail.com>
2025-04-27 01:56:15 +0530

    Add files via upload
    if-else added

---
+++ C_C++/IF-ELSE.C/IfC0ndition.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdio.h>
+int main(){
+    int n ;
+    printf("Enter Number :- ");
+    scanf("%d",&n);
+    if(n%2==0){
+        printf("Even number");
+    }
+    if(n%2!=0){
+        printf("Odd number");
+    }
+    return 0;
+}
